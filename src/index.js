923a10d3d1cdd41f270dc9ff7b8888ef1097f97b
jrbigmon <juniorterriaga0@gmail.com>
2024-05-04 22:04:39 -0300

    feat: injected apollo middleware

--- src/index.js
+++ src/index.js
@@ -1,26 +1,28 @@
 import express from "express";
 import { schema } from "./graphql/graphql.schema.js";
 import { resolvers } from "./graphql/graphql.resolver.js";
-import { graphqlHTTP } from "express-graphql";
 import { dev } from "./db/connection.js";
 import { categoryModel } from "./db/models/category.model.js";
 import { courseModel } from "./db/models/course.model.js";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import cors from "cors";
 
 const db = await dev();
 const app = express();
 const port = 3000;
 
-app.use(
-  "/graphql",
-  graphqlHTTP({
-    schema,
-    rootValue: resolvers({
-      categoryModel: categoryModel(db),
-      courseModel: courseModel(db),
-    }),
-    graphiql: true,
-  })
-);
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers: resolvers({
+    categoryModel: categoryModel(db),
+    courseModel: courseModel(db),
+  }),
+});
+
+await server.start();
+
+app.use("/graphql", cors(), express.json(), expressMiddleware(server));
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
